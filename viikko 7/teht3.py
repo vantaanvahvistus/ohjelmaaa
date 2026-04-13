ea13d30ffbf4b96b7cb7afeee54ffad21ad6a560
vantaanvahvistus <sanni.peppiina@gmail.com>
2026-04-13 19:39:35 +0300

    dfuouyuy

--- viikko 7/teht3.py
+++ viikko 7/teht3.py
@@ -1,25 +1,19 @@
 lentoasemat = {}
 
 while True:
-    print("valitse haluamasi toiminto")
-    print("1, syötä uusi lentoasema: ")
-    print("2, lentoaseman tiedot: ")
-    print("3, lopeta: ")
-
-    valinta = input("valitsit: ")
+    valinta = input("valitse haluamasi toiminto. 1 = syötä uusi asema, 2 = aseman tiedot (tyhjä lopettaa): ")
     if valinta == "1":
         icao = input("anna aseman icao-koodi: ")
         nimi = input("anna aseman nimi: ")
         lentoasemat[icao] = nimi
-        print(f"asema {icao} tallennettu")
+        print("asema tallennettu")
 
     elif valinta == "2":
         icao = input("anna aseman icao-koodi: ")
         if icao in lentoasemat:
             print(f"icao-koodin {icao} asema on: {lentoasemat[icao]} ")
         else:
-            print("koodilla {icao} ei löytynyt sopivaa asemaa")
-    elif valinta == "3":
-        print("ohjelma päättyy")
+            print("koodilla ei asemaa")
+    elif valinta == "":
+        print("lopetetaan")
         break
-
